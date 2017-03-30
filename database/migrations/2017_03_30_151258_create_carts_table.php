22baabbeb04f2cdd9cdbd9efd014c3338d86edd7
Sam Hoang <samhv.ict@gmail.com>
2017-03-30 22:27:27 +0700

    + migrations

--- database/migrations/2017_03_30_151258_create_carts_table.php
+++ database/migrations/2017_03_30_151258_create_carts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateCartsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->boolean('admin');
-            $table->rememberToken();
+        Schema::create('carts', function (Blueprint $table) {
+            $table->integer('book_id')->references('id')->on('books');
+            $table->integer('order_id')->references('id')->on('orders');
+            $table->integer('qty');
+            $table->primary('book_id', 'order_id');
             $table->timestamps();
         });
     }
@@ -31,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('carts');
     }
 }
--- database/migrations/2017_03_30_151327_create_categories_table.php
+++ database/migrations/2017_03_30_151327_create_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->boolean('admin');
-            $table->rememberToken();
+        Schema::create('categories', function (Blueprint $table) {
+            $table->integer('book_id')->references('id')->on('books');
+            $table->integer('category_list_id')->references('id')->on('category_lists');
+            $table->primary('book_id', 'category_list_id');
             $table->timestamps();
         });
     }
@@ -31,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('categories');
     }
 }
