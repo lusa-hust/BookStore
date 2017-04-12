efd3b217af5823c14192d33f2fcee83eef360f75
LuongNV <luongnv@ows.edu.vn>
2017-04-12 23:18:25 +0700

    Mod: change the name of layout view

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -12,6 +12,8 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/layout.css') }}" rel="stylesheet">
+    @yield('style')
 
     <!-- Scripts -->
     <script>
