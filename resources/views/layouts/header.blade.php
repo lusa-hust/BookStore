c3cf005fe3570c5db95ee18c91897ff0381d45b8
LuongNV <luongnv@ows.edu.vn>
2017-04-25 11:18:53 +0700

    Add: authentication link

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -21,18 +21,33 @@
             </form>
         </div>
         <div class="col-xs-4">
-            <div class="pull-right">
-                <ul class="nav pull-right">
-                    <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Welcome, User <b class="caret"></b></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="#"><i class="icon-cog"></i> Shopping cart</a></li>
-                            <li><a href="#"><i class="icon-envelope"></i> Management</a></li>
-                            <li class="divider"></li>
-                            <li><a href="#"><i class="icon-off"></i> Logout</a></li>
-                        </ul>
-                    </li>
-                </ul>
-              </div>
+            <ul class="nav navbar-nav navbar-right">
+                <!-- Authentication Links -->
+                    @if (Auth::guest())
+                        <li><a href="{{ route('login') }}">Login</a></li>
+                        <li><a href="{{ route('register') }}">Register</a></li>
+                    @else
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <ul class="dropdown-menu" role="menu">
+                                <li>
+                                    <a href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();">
+                                        Logout
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        {{ csrf_field() }}
+                                    </form>
+                                </li>
+                            </ul>
+                        </li>
+                    @endif
+            </ul>
         </div>
     </div>
 </nav>
