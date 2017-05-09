a5e13aed187d7358ecd43173c6dcef629eb8c9df
LuongNV <luongnv@ows.edu.vn>
2017-05-09 09:44:59 +0700

    Add: banner of home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,44 @@
 @extends('layouts.layout')
 
 @section('content')
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+        <li data-target="#myCarousel" data-slide-to="3"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+        <div class="item active">
+            <img src="/images/bookcover/banner1.png" alt="Los Angeles">
+        </div>
+
+        <div class="item">
+            <img src="/images/bookcover/banner2.jpg" alt="Chicago">
+        </div>
+
+        <div class="item">
+            <img src="/images/bookcover/banner3.jpg" alt="New York">
+        </div>
+        <div class="item">
+            <img src="/images/bookcover/banner4.jpg" alt="New York">
+        </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
+
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
