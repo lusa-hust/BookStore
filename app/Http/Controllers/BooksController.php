2e7165d2028bbbafa7ee9a571196f4e3ebb14abe
Sam Hoang <samhv.ict@gmail.com>
2017-05-14 13:46:21 +0700

    + POST Method | store Book

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Book;
 use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\ModelNotFoundException as ModelNotFoundException;
+use Exception;
 
 class BooksController extends Controller
 {
@@ -30,18 +32,35 @@ class BooksController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+            'title' => 'required|',
+            'author' => 'required',
+            'price' => 'required|numeric|min:0',
+            'qty' => 'required|integer|min:1',
+            'description' => 'nullable'
+        ]);
+        $book = Book::create([
+            'title' => request('title'),
+            'author' => request('author'),
+            'price' => request('price'),
+            'qty' => request('qty'),
+            'description' => request('description'),
+        ]);
+
+        $book->save();
+
+        return redirect()->back()->with('flash', 'Book has been Created!');
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Book  $book
+     * @param  \App\Book $book
      * @return \Illuminate\Http\Response
      */
     public function show(Book $book)
@@ -53,7 +72,7 @@ class BooksController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Book  $book
+     * @param  \App\Book $book
      * @return \Illuminate\Http\Response
      */
     public function edit(Book $book)
@@ -64,8 +83,8 @@ class BooksController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Book  $book
+     * @param  \Illuminate\Http\Request $request
+     * @param  \App\Book $book
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Book $book)
@@ -76,11 +95,16 @@ class BooksController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Book  $book
+     * @param  \App\Book $book
      * @return \Illuminate\Http\Response
      */
     public function destroy(Book $book)
     {
-        //
+        $book->delete();
+        if (request()->wantsJson()) {
+            return response([], 204);
+        }
+        return redirect()->back();
+
     }
 }
