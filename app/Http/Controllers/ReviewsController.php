5887db52e10b2f96296f4f9b6f03b528e9908f8b
Sam Hoang <samhv.ict@gmail.com>
2017-05-14 18:00:17 +0700

    + reviewController

---
+++ app/Http/Controllers/ReviewsController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Book;
+use App\Review;
+use Illuminate\Http\Request;
+
+class ReviewsController extends Controller
+{
+
+    /**
+     * Create a new ReviewsController instance.
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \App\Book $book
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, Book $book)
+    {
+        $this->validate($request, [
+            'review' => 'required',
+            'vote' => 'required|numeric|min:0'
+        ]);
+        $review = [
+            'review' => request('review'),
+            'vote' => request('vote'),
+            'user_id' => auth()->id()
+        ];
+
+        $book->addReview($review);
+
+        if (request()->wantsJson()) {
+            return response()->json([
+                'status' => true,
+            ], 201);
+        }
+
+        return redirect()->back()->with('flash', 'Review has been Posted!');
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \App\Book $book
+     * @param  \App\Review $review
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Book $book, Review $review)
+    {
+        $this->authorize('update', $review);
+
+        $this->validate($request, [
+            'review' => 'required',
+            'vote' => 'required|numeric|min:0'
+        ]);
+
+        $review->update([
+            'review' => request('review'),
+            'vote' => request('vote')
+        ]);
+
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Book $book
+     * @param  \App\Review $review
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Book $book, Review $review)
+    {
+        $this->authorize('delete', $review);
+        $review->delete();
+
+        if (request()->expectsJson()) {
+            return response(['status' => 'Reply deleted']);
+        }
+
+        return back();
+    }
+}
