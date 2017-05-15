88be7949edd5c6adb586f8987700e7a4575473b2
LuongNV <luongnv@ows.edu.vn>
2017-05-16 02:29:55 +0700

    Add: link to book

--- resources/views/search/searchitem.blade.php
+++ resources/views/search/searchitem.blade.php
@@ -1,8 +1,9 @@
 <div class="search-item">
-    <span class="search-image">
-        <img src="/storage/covers/{{$book->image}}">
-    </span>
-    <span class="search-title">{{$book->title}}</span>
-    <p class="search-price"><b>{{$book->price}}&nbsp;đ</b></p>
-
+	<a href="{{route('books.show', $book->id)}}">
+	    <span class="search-image">
+	        <img src="/storage/covers/{{$book->image}}">
+	    </span>
+	    <span class="search-title">{{$book->title}}</span>
+	    <p class="search-price"><b>{{$book->price}}&nbsp;đ</b></p>
+	</a>
 </div>
